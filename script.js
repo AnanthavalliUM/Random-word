22210be5843afad41bc9997d42c4ce0424a23b69
Ananthavalli <100471735+AnanthavalliUM@users.noreply.github.com>
2023-09-05 15:09:42 +0530

    Add files via upload
    Getting a Random word from api

---
+++ script.js
@@ -0,0 +1,15 @@
+async function get(){              //async 
+    try{
+         var data = await fetch("https://random-words-api.vercel.app/word",{method:"GET"});//fetch with method
+         data=await data.json();
+       }catch(err){console.log(err);}
+        let info=data[0];
+        const div=document.querySelector(".dis");  //geting hold on dis and displaying the fetch data
+        div.innerHTML=""
+        div.innerHTML=`
+        <div id="card">
+        <h1>${info.word}</h1>
+        <p>Meaning: ${info.definition}</>
+        <p>Pronunciation: ${info.pronunciation}</p>
+        </div>`;
+  }
